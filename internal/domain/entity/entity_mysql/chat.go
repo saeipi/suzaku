3c729f37235ec845498f093e20a7a5be4a2c70df
saeipi <ksert@163.com>
2022-04-27 13:03:56 +0800

    消息注解

--- internal/domain/entity/entity_mysql/chat.go
+++ internal/domain/entity/entity_mysql/chat.go
@@ -4,16 +4,24 @@ import (
 	"time"
 )
 
+/*
+serverMsgID生成:
+func GetMsgID(sendID string) string {
+	t := time.Now().Format("2006-01-02 15:04:05")
+	return utils.Md5(t + "-" + sendID + "-" + strconv.Itoa(rand.Int()))
+}
+*/
+
 type ChatLog struct {
-	ServerMsgID      string    `gorm:"column:server_msg_id;primary_key;type:char(64)" json:"serverMsgID"`
-	ClientMsgID      string    `gorm:"column:client_msg_id;type:char(64)" json:"clientMsgID"`
-	SendID           string    `gorm:"column:send_id;type:char(64)" json:"sendID"`
-	RecvID           string    `gorm:"column:recv_id;type:char(64)" json:"recvID"`
-	SenderPlatformID int32     `gorm:"column:sender_platform_id" json:"senderPlatformID"`
+	ServerMsgID      string    `gorm:"column:server_msg_id;primary_key;type:char(64)" json:"serverMsgID"` // 服务端生成
+	ClientMsgID      string    `gorm:"column:client_msg_id;type:char(64)" json:"clientMsgID"`             // 客户端生成
+	SendID           string    `gorm:"column:send_id;type:char(64)" json:"sendID"`                        // 发送人ID
+	RecvID           string    `gorm:"column:recv_id;type:char(64)" json:"recvID"`                        // 发送人ID 或 群ID
+	SenderPlatformID int32     `gorm:"column:sender_platform_id" json:"senderPlatformID"`                 // 发送人平台ID
 	SenderNickname   string    `gorm:"column:sender_nick_name;type:varchar(255)" json:"senderNickname"`
 	SenderFaceURL    string    `gorm:"column:sender_face_url;type:varchar(255)" json:"senderFaceURL"`
-	SessionType      int32     `gorm:"column:session_type" json:"sessionType"`
-	MsgFrom          int32     `gorm:"column:msg_from" json:"msgFrom"`
+	SessionType      int32     `gorm:"column:session_type" json:"sessionType"` // 1:单聊 2:群聊
+	MsgFrom          int32     `gorm:"column:msg_from" json:"msgFrom"`         // 100:用户消息 200:系统消息
 	ContentType      int32     `gorm:"column:content_type" json:"contentType"`
 	Content          string    `gorm:"column:content;type:varchar(3000)" json:"content"`
 	Status           int32     `gorm:"column:status" json:"status"`
