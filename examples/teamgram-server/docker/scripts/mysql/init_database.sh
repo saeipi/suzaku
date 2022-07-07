3c19d9b900c8dd3f3a1ac02492aec2776dfb563e
saeipi <ksert@163.com>
2022-07-07 12:20:28 +0800

    更新文件名

--- examples/teamgram-server/docker/scripts/mysql/init_database.sh
+++ examples/teamgram-server/docker/scripts/mysql/init_database.sh
@@ -4,9 +4,9 @@ MYSQL_USER=${MYSQL_USER:-root}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-teamgram2022}
 MYSQL_DB="teamgram"
 SCRIPT_PATH=$(cd $(dirname $0);pwd)
-SQL_FILE_MIGRATES="/sqls"
+SQL_FILE="/sqls"
 
-folder=$SCRIPT_PATH$SQL_FILE_MIGRATES
+folder=$SCRIPT_PATH$SQL_FILE
 for file in ${folder}/*
 do
   mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -D${MYSQL_DB} < ${file}
