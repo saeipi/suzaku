0a2f13626dab7566f0f7d77ae145d7027d407e9a
saeipi <ksert@163.com>
2022-07-07 12:18:15 +0800

    优化脚本

--- examples/teamgram-server/docker/scripts/mysql/init_database.sh
+++ examples/teamgram-server/docker/scripts/mysql/init_database.sh
@@ -4,13 +4,7 @@ MYSQL_USER=${MYSQL_USER:-root}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-teamgram2022}
 MYSQL_DB="teamgram"
 SCRIPT_PATH=$(cd $(dirname $0);pwd)
-SQL_FILE_INIT_DB="/init/teamgram2.sql"
-SQL_FILE_MIGRATES="/migrates"
-
-CREATE_DATABASE="create database ${MYSQL_DB} default character set utf8mb4 collate utf8mb4_unicode_ci;"
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$CREATE_DATABASE"
-
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -D${MYSQL_DB} < "$SCRIPT_PATH$SQL_FILE_INIT_DB"
+SQL_FILE_MIGRATES="/sqls"
 
 folder=$SCRIPT_PATH$SQL_FILE_MIGRATES
 for file in ${folder}/*
