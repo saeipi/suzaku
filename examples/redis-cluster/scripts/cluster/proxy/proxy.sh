a010b498b4f7f74969e3777e017ea9349ee29f52
saeipi <ksert@163.com>
2022-07-18 20:38:50 +0800

    备份

--- examples/redis-cluster/scripts/cluster/proxy/proxy.sh
+++ examples/redis-cluster/scripts/cluster/proxy/proxy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7001 -j DNAT --to-destination 172.18.0.11:7001
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7002 -j DNAT --to-destination 172.18.0.12:7002
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7003 -j DNAT --to-destination 172.18.0.13:7003
@@ -6,3 +8,5 @@ iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7005 -j DNAT --to-destinatio
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7006 -j DNAT --to-destination 172.18.0.16:7006
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7007 -j DNAT --to-destination 172.18.0.17:7007
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 7008 -j DNAT --to-destination 172.18.0.18:7008
+
+tail -f /dev/null
