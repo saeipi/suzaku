252386e1319a219c2b598c058dbd3364f6413e1a
saeipi <ksert@163.com>
2022-07-18 13:20:07 +0800

    更新脚本

--- examples/redis-cluster/scripts/cluster/redis/redis_config_copy.sh
+++ examples/redis-cluster/scripts/cluster/redis/redis_config_copy.sh
@@ -1,5 +1,10 @@
-for i in `seq 7001 7007`;
+for i in `seq 7001 7008`;
 do
   mkdir -p ./../../../configs/cluster/redis/${i}
-  cp -Rp ./../../../configs/cluster/redis/7000/*  ./../../../configs/cluster/redis/${i}
+  cp -Rp ./../../../configs/cluster/redis.conf  ./../../../configs/cluster/redis/${i}
+  # 换行
+  echo -e >> ./../../../configs/cluster/redis/${i}/redis.conf
+  echo -e "port "${i} >> ./../../../configs/cluster/redis/${i}/redis.conf
 done
+
+chmod -R 7777 ./../../../configs
