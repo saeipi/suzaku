8d5e9b50537fdc8e1274c1bdeef0f443137dd162
saeipi <ksert@163.com>
2022-07-18 14:54:43 +0800

    修改配置

--- examples/redis-cluster/scripts/cluster/redis/redis_cluster_master.sh
+++ examples/redis-cluster/scripts/cluster/redis/redis_cluster_master.sh
@@ -3,8 +3,8 @@ nohup redis-server /usr/local/etc/redis/redis.conf &
 sleep 5
 
 redis-cli --cluster create \
-    172.18.0.11:7001 172.18.0.12:7002 172.18.0.13:7003 172.18.0.14:7004 \
-    172.18.0.15:7005 172.18.0.16:7006 172.18.0.17:7007 172.18.0.18:7008 \
+    redis-node-01:7001 redis-node-02:7002 redis-node-03:7003 redis-node-04:7004 \
+    redis-node-05:7005 redis-node-06:7006 redis-node-07:7007 redis-node-08:7008 \
     --cluster-yes --cluster-replicas 1
 
 tail -f /dev/null
