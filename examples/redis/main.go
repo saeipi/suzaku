053095c285b5a1c4acb2f525da0eb22c6d1cf505
saeipi <ksert@163.com>
2022-07-19 15:10:54 +0800

    redis集群

--- examples/redis/main.go
+++ examples/redis/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v9"
 )
 
 func main() {
+	var ctx = context.Background()
 	client := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"10.0.115.140:6381"},
+		Addrs: []string{
+			"127.0.0.1:7001",
+			"127.0.0.1:7002",
+			"127.0.0.1:7003",
+			"127.0.0.1:7004",
+		},
 	})
-	err := client.Ping().Err()
+	err := client.Ping(ctx).Err()
 	if err != nil {
 		fmt.Print(err.Error())
 	}
 	for i := 0; i < 10; i++ {
 		key := fmt.Sprintf("k%d", i)
 		val := fmt.Sprintf("v%d", i)
-		err = client.Set(key, val, 0).Err()
+		err = client.Set(ctx, key, val, 0).Err()
 		if err != nil {
 			fmt.Print(err.Error())
 		}
-		val, err = client.Get(key).Result()
+		val, err = client.Get(ctx, key).Result()
 		if err != nil {
 			fmt.Print(err.Error())
 		}
