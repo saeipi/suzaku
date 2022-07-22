b6341f88be706880911c38c724926cb6a29a53bd
saeipi <ksert@163.com>
2022-07-22 19:38:05 +0800

    备份

--- examples/regexp/main.go
+++ examples/regexp/main.go
@@ -24,7 +24,7 @@ func main() {
 	list1 := regex.FindAllStringSubmatch(str,-1)
 	fmt.Println(list1)
 
-	// 3 截取取值字段 FindAllString
+	// 4 截取取值字段 FindAllString
 	str = "$main.countDistinct($city)\n$main.countDistinct($user_id)\n$main.countDistinct($age)"
 	regex = regexp.MustCompile("countDistinct\\(\\$(.*?)\\)")
 	list = regex.FindAllString(str,-1)
